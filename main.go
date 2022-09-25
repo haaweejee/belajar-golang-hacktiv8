77ed4c9d7809520519d5e5e1f948eddf726129a8
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2022-09-25 21:39:27 +0700

    ! clear session one hacktiv8

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+/**
+*
+* Function main adalah function yang dipanggil pertama kali pada saat eksekusi program.
+* function ini biasanya berisi fungsi atau logic pemrograman yang akan dieksekusi.
+*
+*
+* Note: untuk menjalankan function silahkan uncomment function nya
+**/
+func main() {
+
+	//Go Programming Introduction - Sesi 1
+	// runFirstProject()
+
+	//Variables - Variable With Data - Sesi 1
+	// variableWithDataType()
+
+	//Variables - Variable Without Data Types - Sesi 1
+	// variableWithoutDataTypes()
+
+	//Variables - Variable Without Data Types (Short Declaration) - Sesi 1
+	// shortDeclarationVariables()
+
+	//Variable - MultipleVariable declarations - Sesi 1
+	// multipleVariableDeclarations()
+
+	//Variable - Underscore Variables - Sesi 1
+	// underscoreVariableDeclarations()
+
+	//Variable - Constant Variable Sesi 1
+	// createConstantVariable()
+
+	//Data Types - Number(Integers) - Sesi 1
+	// usingIntegers()
+
+	//Data Types - Number(Float) - Sesi 1
+	// usingFloat()
+
+	//Data Types - Bool - Sesi 1
+	// usingBool()
+
+	//Data Types - String - Sesi 1
+	// usingStringWithQoute()
+
+	//Data Types - String - Sesi 1
+	// usingStringWithBackticks()
+
+	//Arimethics Operators - Constant & Operators - Sesi 1
+	// arithmathicsOperators()
+
+	//Relational Operators - Constant & Operators - Sesi 1
+	// relationalOperators()
+
+	//Logical Operators - Constant & Operators - Sesi 1
+	logicalOperators()
+
+}
+
+func runFirstProject() {
+	//fmt.Prinln akan menghasilkan baris baru ketika selesai dieksekusi
+	fmt.Println("Hello World!")
+
+	//fmt.Print tidak akan menghasilkan baris baru ketika selesai di eksekusi
+	fmt.Print("Nama Saya ")
+	fmt.Print("Hendrawan Wibowo")
+}
