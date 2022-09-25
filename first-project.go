b644611747790a52b8b0c2f12a40758d0b6486bd
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2022-09-25 17:55:47 +0700

    + add comment

--- first-project.go
+++ first-project.go
@@ -2,8 +2,18 @@ package main
 
 import "fmt"
 
+/**
+*
+* Function main adalah function yang dipanggil pertama kali pada saat eksekusi program.
+* function ini biasanya berisi fungsi atau logic pemrograman yang akan dieksekusi.
+*
+**/
 func main() {
+
+	//fmt.Prinln akan menghasilkan baris baru ketika selesai dieksekusi
 	fmt.Println("Hello World!")
+
+	//fmt.Print tidak akan menghasilkan baris baru ketika selesai di eksekusi
 	fmt.Print("Nama Saya ")
 	fmt.Print("Hendrawan Wibowo")
 }
